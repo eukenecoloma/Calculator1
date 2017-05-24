4ed1738b6e7d84fdf1241c659db06c312b7cca94
gregohp <grego@hemen.eu>
2017-05-24 19:08:42 +0200

    Changes

--- app/src/main/java/innova4b/com/calculator/MainActivity.java
+++ app/src/main/java/innova4b/com/calculator/MainActivity.java
@@ -2,12 +2,31 @@ package innova4b.com.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView screenTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        screenTextView = (TextView)findViewById(R.id.screen);
+
     }
+
+
+    public void numericButtonClicked(View view){
+        Button numericButton = (Button) view;
+        String numberStr = numericButton.getText().toString();
+        screenTextView.setText(screenTextView.getText() + numberStr);
+
+    }
+
+
+
+
 }
